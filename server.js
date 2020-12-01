ee89fae96370b4ec028eca5c5a0813d7ace6d094
Roger Minton <rminton@qinship1.com>
2020-12-01 11:25:56 -0500

    Initial Commit

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // server.js
 // where your node app starts
-
+require('dotenv').config();
+var moment = require('moment');
 // init project
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,11 +20,20 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp/:userTimestamp", function(req,res){
+  const userTime = req.params.userTimestamp;
+  const momentTime = isNaN(+userTime) ?  moment(userTime) : moment.unix(+userTime);
+  res.json({
+    unix: momentTime.unix(),
+    utc: momentTime.utc(),
+  });
+
+});
 
 
 // listen for requests :)
